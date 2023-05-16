eb4ae5db9c8d5a8df14f44572f87c0625d670cc7
Hyunjeong Choi <hyunjeongchoi@hyunjeongs-mini.home>
2023-05-16 14:35:26 -0400

    updated to use authenticate middleware for all of the /v1 routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const { version, author } = require('../../package.json');
+const { authenticate } = require('../authentication'); // authenticate middleware
 
 // Create a router that we can use to mount our API
 const router = express.Router();
 
 // Expose all of our API routes on /v1/* to include an API version.
-router.use(`/v1`, require('./api'));
+// protect them all, so you have to be authenticated in order to access
+router.use(`/v1`, authenticate(), require('./api'));
 
-/**
- * Define a simple health check route. If the server is running
- * we'll respond with a 200 OK.  If not, the server isn't healthy.
- */
+// Define a simple health check route. If the server is running
 router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
