3756835d0811eaf1e617798d784ddf6f89577754
Hyunjeong Choi <hyunjeongchoi@hyunjeongs-mini.home>
2023-05-16 10:36:49 -0400

    added directory and separate files to avoid having src/app.js get too big

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { version, author } = require('../../package.json');
+
+// Create a router that we can use to mount our API
+const router = express.Router();
+
+// Expose all of our API routes on /v1/* to include an API version.
+router.use(`/v1`, require('./api'));
+
+/**
+ * Define a simple health check route. If the server is running
+ * we'll respond with a 200 OK.  If not, the server isn't healthy.
+ */
+router.get('/', (req, res) => {
+  // Client's shouldn't cache this response (always request it fresh)
+  res.setHeader('Cache-Control', 'no-cache');
+  res.status(200).json({
+    status: 'ok',
+    author,
+    githubUrl: 'https://github.com/avelynhc/fragments',
+    version,
+  });
+});
+
+module.exports = router;
