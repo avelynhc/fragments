86ddf375f944db1252a1986690d5e576fc251afd
Avelyn Choi <avelynhc@gmail.com>
2023-05-24 18:37:15 -0400

    Updated to reuse createSuccessResponse function

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,8 +1,12 @@
+const { createSuccessResponse } = require('../../response');
+
 // Get a list of fragments for the current user
 module.exports = (req, res) => {
   // TODO: this is just a placeholder to get something working...
-  res.status(200).json({
+  const data = {
     status: 'ok',
     fragments: [],
-  });
+  }
+  const successResponse = createSuccessResponse(data);
+  res.status(200).json(successResponse);
 };
