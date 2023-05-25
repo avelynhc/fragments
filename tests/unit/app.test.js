724be9be2198240435180eb909e61ac5f3d104aa
Avelyn Choi <avelynhc@gmail.com>
2023-05-25 16:52:44 -0400

    Updated description of testing

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-describe('/ app not found', () => {
+describe('/ Invalid endpoint', () => {
   test('should return HTTP 404 response', async () => {
     const res = await request(app).get('/hello');
     expect(res.status).toBe(404);
