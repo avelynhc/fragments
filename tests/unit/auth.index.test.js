ee36385053e4c98f5cf62bc14f6a6b166ce57558
Avelyn Choi <avelynhc@gmail.com>
2023-05-25 16:17:23 -0400

    Added jest test to see if it throws an error when invalid env vars are provided

---
+++ tests/unit/auth.index.test.js
@@ -0,0 +1,19 @@
+describe('When env vars are invalid', () => {
+  // reset the module to delete possible cached info in jest on each
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('throws an error', async () => {
+    // reset env vars
+    process.env = {
+      ...(process.env || {}),
+      AWS_COGNITO_CLIENT_ID: '',
+      AWS_COGNITO_POOL_ID: '',
+      HTPASSWD_FILE: '',
+    };
+    expect(() => {
+      require('../../src/authorization'); // call src/auth/index.js file
+    }).toThrow(new Error('missing env vars: no authorization configuration found'));
+  });
+});
