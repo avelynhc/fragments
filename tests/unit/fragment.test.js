e5edaac6164e930fc922a54f34921d16b6a90369
Avelyn Choi <avelynhc@gmail.com>
2023-05-29 22:37:36 -0400

    Added unit test for Fragment class

--- tests/unit/fragment.test.js
+++ tests/unit/fragment.test.js
@@ -173,18 +173,16 @@ describe('Fragment class', () => {
       expect(await Fragment.byUser('1234')).toEqual([]);
     });
 
-    // test('a fragment can be created and save() stores a fragment for the user', async () => {
-    //   const data = Buffer.from('hello');
-    //   const fragment = new Fragment({
-    //                               ownerId: '1234', type: 'text/plain', size: 0 });
-    //   await fragment.save();
-    //   await fragment.setData(data);
-    //   console.log(fragment);
-    //
-    //   const fragment2 = await Fragment.byId('1234', fragment.id);
-    //   expect(fragment2).toEqual(fragment);
-    //   expect(await fragment2.getData()).toEqual(data);
-    // });
+    test('a fragment can be created and save() stores a fragment for the user', async () => {
+      const data = Buffer.from('hello');
+      const fragment = new Fragment({
+                                  ownerId: '1234', type: 'text/plain', size: 0 });
+      await fragment.save();
+      await fragment.setData(data);
+      const fragment2 = await Fragment.byId('1234', fragment.id);
+      expect(fragment2).toEqual(fragment);
+      expect(await fragment2.getData()).toEqual(data);
+    });
 
     // test('save() updates the updated date/time of a fragment', async () => {
     //   const ownerId = '7777';
