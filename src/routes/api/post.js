4dda9eb58eb971b90b79ef78b3f3511e0b9dc4a6
Avelyn Choi <avelynhc@gmail.com>
2023-05-30 22:38:19 -0400

    Updated to use req.headers.host to set response's headers for location

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,15 +1,14 @@
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 const { createSuccessResponse, createErrorResponse } = require("../../response");
-require('dotenv').config();
 
 module.exports = async (req, res) => {
   logger.debug({ req }, '/POST request');
 
-  if(!process.env.API_URL) {
-    logger.warn('missing expected env var: API_URL');
-    throw new Error('missing expected env var: API_URL');
-  }
+  // if(!process.env.API_URL) {
+  //   logger.warn('missing expected env var: API_URL');
+  //   throw new Error('missing expected env var: API_URL');
+  // }
 
   if(!Buffer.isBuffer(req.body)) {
     logger.warn({ req }, 'Given data is not a Buffer');
@@ -26,10 +25,11 @@ module.exports = async (req, res) => {
     logger.info({ fragment }, 'Successfully created a new fragment');
 
     // setting the response's HTTP headers field
-    console.log(req.headers.host);
     res.set({
       // location will use req.headers.host when API_URL is missing on AWS
-      'Location': `${process.env.API_URL}/v1/fragments/${fragment.id}`,
+      // use http://localhost:8080 when running locally
+      // 'Location': `${process.env.API_URL}/v1/fragments/${fragment.id}`,
+      'Location': `http://${req.headers.host}/v1/fragments/${fragment.id}`,
       'Content-Type': fragment.type
     });
     logger.info({ fragment }, 'Successfully setting the header');
