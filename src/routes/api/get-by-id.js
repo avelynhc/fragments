f1b8414d4708bb243cae9e407b80c9ce1a5ed426
Avelyn Choi <avelynhc@gmail.com>
2023-05-31 15:20:47 -0400

    Removed unneccary codes

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -5,13 +5,8 @@ const { createErrorResponse, createSuccessResponse } = require('../../response')
 // Get an existing fragment
 module.exports = async (req, res) => {
   logger.debug({ req }, 'GET /fragments/:id');
-
   try {
     const fragment =  await Fragment.byId(req.user, req.params.id);
-    if(!fragment) {
-      logger.warn({ req }, 'Cannot find fragment with the given id')
-      return res.status(404).json(createErrorResponse(404, 'Cannot find fragment with the given id'));
-    }
     logger.info({ fragment }, 'Successfully retrieve an existing fragment based on the given id');
     res.status(200).json(createSuccessResponse(fragment));
   } catch(error) {
