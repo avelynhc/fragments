e600210f003ebfca64d1b074f4c7cba089152206
Avelyn Choi <avelynhc@gmail.com>
2023-06-08 13:09:02 -0400

    Updated logger.debug elements

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -4,7 +4,9 @@ const { createErrorResponse, createSuccessResponse } = require('../../response')
 
 // Get an existing fragment
 module.exports = async (req, res) => {
-  logger.debug({ req }, 'GET /fragments/:id');
+  const userInfo = req.user;
+  const idParam = req.params.id;
+  logger.debug({ userInfo, idParam }, 'GET /fragments/:id');
   try {
     const fragment =  await Fragment.byId(req.user, req.params.id);
     logger.info({ fragment }, 'Successfully retrieve an existing fragment based on the given id');
