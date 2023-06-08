6bfe9a145050e55958ae7912930555792ddc522f
Avelyn Choi <avelynhc@gmail.com>
2023-06-08 10:45:50 -0400

    Added GET /fragments/?expand=1 route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,11 +1,20 @@
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const logger = require('../../logger');
+const { Fragment } = require('../../model/fragment');
 
 // Get a list of fragments for the current user
-module.exports = (req, res) => {
-  const data = {
-    status: 'ok',
-    fragments: [],
+module.exports = async (req, res) => {
+  logger.debug(`Query string - ${req.query}`);
+  // GET /fragments?expand=1
+  const expandValue = req.query.expand && req.query.expand === 1;
+  try {
+    const fragmentsMetadata = await Fragment.byUser(req.user, expandValue);
+    logger.info({ fragmentsMetadata }, 'Successfully GET a list of fragments metadata');
+    res.status(200).json(createSuccessResponse( {
+      fragments: fragmentsMetadata
+    }));
+  } catch(error) {
+    logger.error({ error }, 'GET /fragments/?expand=1 error');
+    res.status(500).json(createErrorResponse(500, error));
   }
-  const successResponse = createSuccessResponse(data);
-  res.status(200).json(successResponse);
 };
