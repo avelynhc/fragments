538948e83085dc3baf0b57ab9b2bd3af51016652
Avelyn Choi <avelynhc@gmail.com>
2023-06-08 21:27:14 -0400

    Updated unit test for get-by-id route

--- tests/unit/get-by-id.test.js
+++ tests/unit/get-by-id.test.js
@@ -41,14 +41,65 @@ describe('GET /v1/fragments/:id', () => {
       .get(`/v1/fragments/${id}`)
       .auth('avelynhc@gmail.com', 'Mustard123!');
     expect(getRes.statusCode).toBe(200);
-    expect(getRes._body.fragments.id).toEqual(id);
     expect(getRes.body.status).toBe('ok');
   });
 
-  test('authenticated users with invalid fragment will throw', async () => {
+  test('authenticated users with invalid id will throw', async () => {
     const getRes = await request(app)
       .get('/v1/fragments/invalidId')
       .auth('avelynhc@gmail.com', 'Mustard123!');
-    expect(getRes.statusCode).toBe(500);
+    expect(getRes.statusCode).toBe(404);
+  });
+});
+
+describe('GET /v1/fragments/:id with optional extension', () => {
+  test('authenticated users can get the fragment based on given id with optional extension', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('avelynhc@gmail.com', 'Mustard123!')
+      .set('Content-Type', 'text/plain')
+      .send('fragment from post request');
+
+    const id = JSON.parse(res.text).fragments.id;
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.txt`)
+      .auth('avelynhc@gmail.com', 'Mustard123!');
+    expect(getRes.statusCode).toBe(200);
+    expect(JSON.parse(getRes.text).status).toBe('ok');
+    expect(JSON.parse(getRes.text).fragments).toBe('fragment from post request');
+  });
+
+  test('authenticated users with invalid fragment will return 404', async () => {
+    const getRes = await request(app)
+      .get('/v1/fragments/invalidId')
+      .auth('avelynhc@gmail.com', 'Mustard123!');
+    expect(getRes.statusCode).toBe(404);
+  });
+
+  test('authenticated users can get the fragment based on given id with optional extension', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('avelynhc@gmail.com', 'Mustard123!')
+      .set('Content-Type', 'text/plain')
+      .send('fragment from post request');
+
+    const id = JSON.parse(res.text).fragments.id;
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.txt`)
+      .auth('avelynhc@gmail.com', 'Mustard123!');
+    expect(getRes.statusCode).toBe(200);
+  });
+
+  test('authenticated users can get the fragment based on given id with optional extension', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('avelynhc@gmail.com', 'Mustard123!')
+      .set('Content-Type', 'text/plain')
+      .send('fragment from post request');
+    const id = JSON.parse(res.text).fragments.id;
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('avelynhc@gmail.com', 'Mustard123!');
+    expect(getRes.statusCode).toBe(415);
   });
 });
