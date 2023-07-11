1d8fad270f1335dfb2d990b9f4adff16b67c4116
Avelyn Choi <avelynhc@gmail.com>
2023-07-11 16:48:40 -0400

    Added code to use either in memory or aws s3 depends on AWS_REGION env variable set

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,2 +1,3 @@
-// Add code to pick the appropriate back-end data strategy
-module.exports = require('./memory');
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
