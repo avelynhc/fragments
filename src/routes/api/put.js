d34702602ab715f7c4854a8361fb2c4b73e35274
Avelyn Choi <avelynhc@gmail.com>
2023-07-19 20:35:01 -0400

    Fixed bugs on put route

--- src/routes/api/put.js
+++ src/routes/api/put.js
@@ -5,6 +5,9 @@ const path = require('node:path');
 
 // Update an existing fragment
 module.exports = async (req, res) => {
+  const reqBody = req.body;
+  logger.debug(req, 'req.body');
+
   if(!process.env.API_URL) {
     logger.warn('missing expected env var: API_URL');
     throw new Error('missing expected env var: API_URL');
@@ -12,24 +15,25 @@ module.exports = async (req, res) => {
 
   if(!Buffer.isBuffer(req.body)) {
     logger.warn('Given data is not a Buffer');
-    return res.status(415).json(createErrorResponse(415, 'Given data is not a Buffer'));
+    return res.status(415).json(createErrorResponse(
+      415, 'Given data is not a Buffer'));
   }
 
   const userInfo = req.user;
   const idParam = req.params.id;
   logger.debug({ userInfo, idParam }, 'PUT /fragments/:id');
-  console.log(userInfo, idParam);
+
   try {
     // check if id includes an optional extension
     const optionalExtension = path.extname(idParam);
     logger.debug({ optionalExtension }, 'Optional extension');
 
     const fragment =  await Fragment.byId(userInfo, path.basename(idParam, optionalExtension));
-    logger.info({ fragment }, 'Successfully retrieve an existing fragment based on the given id');
-    console.log(fragment);
+    logger.info({ fragment }, 'Successfully retrieved an existing fragment based on the given id');
+
     if(!fragment) {
       return res.status(404).json(createErrorResponse(
-        415, 'No such fragment exists with the given id'));
+        404, 'No such fragment exists with the given id'));
     }
 
     if(req.get('Content-Type')!==fragment.type) {
@@ -40,12 +44,15 @@ module.exports = async (req, res) => {
     }
 
     const updatedData = new Fragment({
-      ownerId: req.user,
       id: idParam,
+      ownerId: req.user,
       created: fragment.created,
       type: req.get('Content-Type')
     });
+
+    logger.debug({ updatedData }, 'updated data');
     await updatedData.save();
+    logger.debug('Saving the updated data...');
     await updatedData.setData(req.body);
     logger.info({ updatedData }, 'Successfully updated the existing data');
 
@@ -53,6 +60,7 @@ module.exports = async (req, res) => {
       'Location': `${process.env.API_URL}/v1/fragments/${updatedData.id}`
     });
     logger.info({ updatedData }, 'Successfully setting the header');
+
     res.status(200).json(createSuccessResponse({
       fragments: updatedData
     }));
