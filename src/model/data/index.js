e4201d77ff80e20620ac5a6f3e4e29b8d32900e6
Avelyn Choi <avelynhc@gmail.com>
2023-07-23 15:48:28 -0400

    Updated comments

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,6 +1,3 @@
 // If the environment sets an AWS Region, we'll use AWS backend
 // services (S3, DynamoDB); otherwise, we'll use an in-memory db.
 module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
-
-// testing locally
-// module.exports = require('./memory');
